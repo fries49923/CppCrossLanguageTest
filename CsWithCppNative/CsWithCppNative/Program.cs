179ceabefc794aec1ee8b6163bd7b98ef419cb73
ポテト <fries49923@gmail.com>
2025-04-27 00:01:21 +0800

    Add CsWithCppNative
    1. Add CsWithCppNative

---
+++ CsWithCppNative/CsWithCppNative/Program.cs
@@ -0,0 +1,62 @@
+﻿using static CsWithCppNative.SimpleNativeInterop;
+
+namespace CsWithCppNative
+{
+    public class Program
+    {
+        private static void OnAddResult(int result)
+        {
+            Console.WriteLine($"Callback result: {result}");
+        }
+
+        public static void Main(string[] args)
+        {
+            int result = MathOperationMultiply(5, 10);
+            Console.WriteLine($"5 * 10 = {result}");
+
+            var callback = new TestCallback(OnAddResult);
+
+            MathOperationAdd(7, 8, callback);
+
+            var paramsStruct = new AddParams
+            {
+                a = 3,
+                b = 4
+            };
+
+            MathOperationAdd(paramsStruct, callback);
+
+            //-----------------------------------
+
+            Console.WriteLine($"Current global variable: {GetGlobalVariable()}");
+            SetGlobalVariable(100);
+            Console.WriteLine($"Updated global variable: {GetGlobalVariable()}");
+
+            //-----------------------------------
+
+            var data = new UpdateData
+            {
+                value = 0
+            };
+
+            Console.WriteLine("Initial value: " + data.value);
+            UpdateValueAfterDelay(ref data);
+            Console.WriteLine("Updated value: " + data.value);
+
+            //-----------------------------------
+
+            var mathOps = new MathOperations();
+            InitMathOperations(ref mathOps);
+
+            int a = 10;
+            int b = 5;
+
+            Console.WriteLine($"Add: {mathOps.Add(a, b)}");
+            Console.WriteLine($"Subtract: {mathOps.Subtract(a, b)}");
+            Console.WriteLine($"Multiply: {mathOps.Multiply(a, b)}");
+            Console.WriteLine($"Divide: {mathOps.Divide(a, b)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CsWithCppNative/CsWithCppNative/SimpleNativeInterop.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CsWithCppNative
+{
+    internal static class SimpleNativeInterop
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct AddParams
+        {
+            public int a;
+            public int b;
+        }
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "math_operation_multiply",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern int MathOperationMultiply(int a, int b);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate void TestCallback(int result);
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "math_operation_add",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern void MathOperationAdd(int a, int b, TestCallback callback);
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "math_operation_add_struct",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern void MathOperationAdd(AddParams paramsStruct, TestCallback callback);
+
+        //-----------------------------------
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "get_global_variable",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern int GetGlobalVariable();
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "set_global_variable",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern void SetGlobalVariable(int value);
+
+        //-----------------------------------
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct UpdateData
+        {
+            public int value;
+        }
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "update_value_after_delay",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern void UpdateValueAfterDelay(ref UpdateData data);
+
+        //-----------------------------------
+
+        public struct MathOperations
+        {
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate int DelegateAdd(int a, int b);
+            public DelegateAdd Add;
+
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate int DelegateSubtract(int a, int b);
+            public DelegateSubtract Subtract;
+
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate int DelegateMultiply(int a, int b);
+            public DelegateMultiply Multiply;
+
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate int DelegateDivide(int a, int b);
+            public DelegateDivide Divide;
+        }
+
+        [DllImport("CppNativeLib.dll",
+            EntryPoint = "init_math_operations",
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern void InitMathOperations(ref MathOperations mathOps);
+    }
+}
